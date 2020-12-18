34d3923b7f3dc8696141befa4d7975e281744a21
Shauray <shauray9@gmail.com>
2020-12-18 11:37:15 +0530

    Code for Day18

---
+++ day18/first.py
@@ -0,0 +1,29 @@
+lis = []
+w = open('data.txt','r')
+lis.append(w.read())
+lis = lis[0].split("\n")
+lis.pop(-1)
+#print(lis)
+
+temp = ["5 + (8 * 3 + 9 + 3 * 4 * 3)"]
+
+a = []
+for i in temp[0].split(' '):
+    a.append(i)
+
+
+import re
+
+class num(int):
+    def __sub__(self,b):
+        return num(int(self)*int(b))
+
+    def __add__(self, b):
+        return num(int(self) + int(b))
+
+s = 0   
+for row in lis:
+    row = row.replace("*","-")
+    row = re.sub('(\d+)',r'num(\1)', row)
+    s += eval(row)
+print(s)
