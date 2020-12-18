d3c3995f76d098912e443f863c81d90186ef522f
Shauray <shauray9@gmail.com>
2020-12-18 20:42:29 +0530

    Code for Day8 first part done working on part 2

---
+++ day8/first.py
@@ -0,0 +1,23 @@
+lis = []
+w = open('data.txt','r')
+lis.append(w.read())
+lis = lis[0].split("\n")
+lis.pop(-1)
+acc = 0
+bb = True
+i = 0
+check = []
+while(bb):
+    #print(i,lis[i][:3])
+    if lis[i][:3] == "acc":
+        #print(lis[i].split(" ")[1])
+        acc += int(lis[i].split(" ")[1])
+        check.append(i)
+    elif lis[i][:3] == 'jmp':
+        i = i + eval(lis[i].split(" ")[1])-1
+        #print("good",i)
+    i += 1
+    if i in check:
+        bb = False
+print(acc)
+
