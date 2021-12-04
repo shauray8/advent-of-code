283086183e9ace79c81053499b95a76de550592d
Shauray <shauray9@gmail.com>
2021-12-04 10:10:03 +0530

    prep for day 3

--- aoc_21/day3/first.py
+++ aoc_21/day3/first.py
@@ -3,29 +3,17 @@ fn =  "data.in"
 tfn = "try.in"
 lis = open(fn).read().strip().split("\n")
 
-epsilon = ""
-gamma = ""
-for j in range(len(lis[0])):
-    ones, zeros = [], []
-    for i in range(len(lis)):
-        if int(lis[i][j]) == 1:
-            ones.append(1)
-        else:
-            zeros.append(0)
-    print(len(ones))
-    print(len(zeros))
+forward, depth = 0,0
 
-    if len(ones) > len(zeros):
-        gamma += str(1)
-    else:
-        gamma += str(0)
-
-    if len(ones) < len(zeros):
-        epsilon += str(1)
-    else:
-        epsilon += str(0)
+for i in lis:
+    op, nu = i.split(" ")
+    if op == "forward":
+        forward += int(nu)
+    elif op == "down":
+        depth += int(nu)
+    else: 
+        depth -= int(nu)
 
 
-
-print(epsilon, gamma)
+print(forward*depth)
 
--- aoc_21/day3/second.py
+++ aoc_21/day3/second.py
@@ -3,55 +3,19 @@ fn =  "data.in"
 tfn = "try.in"
 lis = open(fn).read().strip().split("\n")
 
-epsilon = ""
-gamma = ""
-l = len(lis[0])
+forward, depth, aim = 0,0,0
 
-for j in range(l):
-    ones, zeros = [], []
-    oneh, zeroh = [], []
+for i in lis:
+    op, nu = i.split(" ")
+    if op == "forward":
+        forward += int(nu)
+        depth += aim*int(nu)
+    elif op == "down":
+        aim += int(nu)
+    else: 
+        aim -= int(nu)
 
-    for i in range(len(lis)):
-        if int(lis[i][j]) == 1:
-            ones.append(1)
-            oneh.append(lis[i])
-        else:
-            zeros.append(0)
-            zeroh.append(lis[i])
-    print(len(ones))
-    print(len(zeros))
 
-    if len(ones) > len(zeros):
-        lis = oneh
-    else:
-        lis = zeroh
-
-    if len(lis) == 1:
-        print("done")
-    print(lis)
-
-for j in range(l):
-    ones, zeros = [], []
-    oneh, zeroh = [], []
-
-    for i in range(len(lis)):
-        if int(lis[i][j]) == 1:
-            ones.append(1)
-            oneh.append(lis[i])
-        else:
-            zeros.append(0)
-            zeroh.append(lis[i])
-    print(len(ones))
-    print(len(zeros))
-
-    if len(ones) < len(zeros):
-        lis = oneh
-    else:
-        lis = zeroh
-
-    if len(lis) == 1:
-
-        print(lis)
-        break
+print(forward*depth)
 
 
