12bf2f478eb6d3cf9fc912d55b37431c88505d77
Shauray <shauray9@gmail.com>
2021-12-06 10:23:36 +0530

    prep for day6

---
+++ aoc_21/day6/first.py
@@ -0,0 +1,15 @@
+import os
+fn =  "data.in"
+tfn = "try.in"
+lis = open(fn).read().strip().split("\n")
+    
+
+
+    
+            
+                
+
+
+
+
+
---
+++ aoc_21/day6/second.py
@@ -0,0 +1,5 @@
+import os
+import numpy as np
+fn =  "data.in"
+tfn = "try.in"
+lis = open(fn).read().strip().split("\n")
