c1bc975f8660e5733b031ea016ad3e400b2d2e11
Shauray <shauray9@gmail.com>
2023-01-02 16:58:12 +0530

    getting 2 problem done

--- striver/1/another.py
+++ striver/1/another.py
@@ -1,4 +1,4 @@
-matrix = [[0,1,2,0],[3,4,5,2],[1,2,1,5]]
+matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
 
 ## first method
 def first(matrix):
@@ -7,15 +7,14 @@ def first(matrix):
             if matrix[i][j] == 0:
                 for k in range(len(matrix)):
                     if matrix[k][j] != 0:
-                        matrix[k][j] = -1
+                        matrix[k][j] = "-1"
                 for k in range(len(matrix[0])):
                     if matrix[i][k] != 0:
-                        print(matrix[i][k])
-                        matrix[i][k] = -1
+                        matrix[i][k] = "-1"
 
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            if matrix[i][j] == -1:
+            if matrix[i][j] == "-1":
                 matrix[i][j] = 0
 
     return matrix
@@ -37,7 +36,29 @@ def second(matrix):
     print(matrix)
 
 
-second(matrix)
+def third(matrix):
+    col0 = 1
+    for i in range(len(matrix)):
+        if matrix[i][0] == 0 : col0 = 0
+        for j in range(len(matrix[0])):
+            if matrix[i][j] == 0:
+                matrix[i][0] = 0
+                matrix[0][j] = 0
+
+    for i in range(len(matrix)-1,0,-1):
+        for j in range(len(matrix[0])-1,0,-1):
+            if matrix[i][0] == 0 or matrix[0][j] == 0:
+                print(i,j)
+                matrix[i][j] = 0
+
+        if col0 == 0:
+            print(i)
+            matrix[i][0] = 0
+
+    print(matrix)
+
+
+third(matrix)
 
 
 
--- striver/1/sol.cpp
+++ striver/1/sol.cpp
@@ -51,10 +51,35 @@ int secoond(vector< vector <int>> & matrix){
   
 }
 
+int third(vector< vector <int>> & matrix){
+  int col0 = 1;
+  for(int i=0; i<matrix.size();i++){
+    if(matrix[i][0] == 0){col0 = 0;}
+    for(int j=1;j<matrix[0].size();j++){
+      if(matrix[i][j] == 0){
+        matrix[i][0] = 0;
+        matrix[0][j] = 0;
+      }
+    }
+  }
+
+ for(int i=matrix.size()-1; i>=0; --i){
+    for(int j=matrix[0].size()-1; j>=0;--j){
+      if(matrix[i][0] == 0 || matrix[0][j] == 0){
+        matrix[i][j] = 0;
+      }
+    }
+    if(col0 == 0){
+      matrix[i][0] = 0;
+    }
+  }
+
+}
+
 int main(){
   vector< vector <int>> arr;
-  arr = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
-  secoond(arr);
+  arr = {{1,2,3,4},{5,0,7,8},{0,10,11,12},{13,14,15,0}};
+  third(arr);
   for(int i=0; i<arr.size(); i++){
     for(int j=0; j<arr[0].size(); j++){
       cout << arr[i][j] << " ";
