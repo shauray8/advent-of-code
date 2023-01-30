2140ef97f5e4e43dfc8e638a304b61c6b3c7009f
Shauray <shauray9@gmail.com>
2023-01-30 14:33:39 +0530

    more solutions

--- codeforces/typeDB/a.py
+++ codeforces/typeDB/a.py
@@ -1,5 +1,6 @@
 from math import log
 
+## I don't using log and making it complex makes sense !
 def solve():
     for i in range(1,int(x)+2):
         for j in range(i,int(x)+2):
@@ -9,13 +10,18 @@ def solve():
     print(-1)
     return
 
+## this shit also works !
+def another():
+    print(-1)
+    return 
+
 if __name__ == "__main__":
     n = int(input())
     while(n):
         x = int(input())
         if x%2 != 0:
             x = log(x)
-            solve()
+            another()
         else:
             print(1,x//2)
         n-=1
